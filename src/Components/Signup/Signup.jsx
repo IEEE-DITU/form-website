dc386297164de07f78201d12726782c5b4c6b6e7
shreyal0007 <shreyaljain0007@gmail.com>
2022-12-23 00:42:23 +0530

    hide and show password

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import authImg from '../../images/authImg.png'
+import eyeclose from "../../images/eye-close.png"
+import eyeopen from "../../images/eye-open.png"
 import './Signup.css'
 import { Link } from 'react-router-dom'
 
 
+
 function Signup() {
+  const[state , setstate] = useState(false);
+  const toggleBtn = () =>{
+    setstate(prevState => !prevState);
+  }
   return (
     <div className='signup'>
       <div>
@@ -20,8 +27,25 @@ function Signup() {
           <label className="userheading" for="user">Username</label>
           <input className="userarea" type="text" placeholder="&#xf007;   Enter your Username" style={{ fontFamily: "Arial, FontAwesome" }} name="user" required />
           <label className="passwordheading" for="password">Password</label>
-          <input className="passwordarea" type="password" placeholder="&#xf023;   Enter your password" style={{ fontFamily: "Arial, FontAwesome" }} name="password" required />
+          <div class="pass">
+            <input className="passwordarea" type={state ? "text": "password"} placeholder="&#xf023;   Enter your password >" style={{ fontFamily: "Arial, FontAwesome" }} name="password" required />
+          
+            <button className='eyeclose' onClick={toggleBtn}>
+              {state ? <img src={eyeopen} className="eyeclose1"></img> : <img src={eyeclose} className="eyeclose1"></img>}
+              
+            </button> 
+          </div>
+          
+          
           <label className="cfmpasswordheading" for="cfmpassword">Confirm Password</label>
+          <div class="pass">
+            <input className="passwordarea" type={state ? "text": "password"} placeholder="&#xf023;   Enter your password >" style={{ fontFamily: "Arial, FontAwesome" }} name="password" required />
+          
+            <button className='eyeclose' onClick={toggleBtn}>
+              {state ? <img src={eyeopen} className="eyeclose1"></img> : <img src={eyeclose} className="eyeclose1"></img>}
+              
+            </button> 
+          </div>
           <input className="cfmpasswordarea" type="password" placeholder="&#xf023;   Enter your password" style={{ fontFamily: "Arial, FontAwesome" }} name="cfmpassword" required />
         </div>
         <button className="registerbtn">Register</button>
