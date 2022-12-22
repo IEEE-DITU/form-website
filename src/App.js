03b65fb01a9635298a4887d3ee99e22f21c0a5a1
shreyal0007 <shreyaljain0007@gmail.com>
2022-12-22 16:16:57 +0530

    sign up page created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Login from './Components/Login/Login';
+import Signup from './Components/Signup/Signup'
 import {
   BrowserRouter as Router,
   Routes,
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
         </Routes>
       </Router>
     </div>
