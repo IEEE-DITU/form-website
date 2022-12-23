cad51c482093754a1223fd544db7f9426a42e961
Vaibhav Aswal <vbhv.aswal@gmail.com>
2022-12-23 18:45:31 +0530

    added response page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
-import Login from './Components/Login/Login';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Signup from './Components/Signup/Signup';
-import Home from './pages/Home/Home';
+import "./App.css";
+import Login from "./Components/Login/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./Components/Signup/Signup";
+import Home from "./pages/Home/Home";
+import ResponsePage from "./pages/ResponsePage/ResponsePage";
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/" element={<Home/>}/>
-        </Routes>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/" element={<Home />} />
+					<Route path="/forms/responses" element={<ResponsePage />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
