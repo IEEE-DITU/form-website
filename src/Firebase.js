a1b8bf2b8d3f089f9b61fb70fcddab2a950c2aea
Vaibhav Aswal <vbhv.aswal@gmail.com>
2022-12-26 23:08:23 +0530

    adding data to db, removed console warnings

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,18 +1,20 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEdGs-i9GxrkLeqb0q0n7-Ec2SwGgEvxI",
-  authDomain: "ieee-custom-forms-181e4.firebaseapp.com",
-  projectId: "ieee-custom-forms-181e4",
-  storageBucket: "ieee-custom-forms-181e4.appspot.com",
-  messagingSenderId: "182981659642",
-  appId: "1:182981659642:web:5b996efc9b10edca543243",
-  measurementId: "G-NJTMLT75G5"
+	apiKey: "AIzaSyCEdGs-i9GxrkLeqb0q0n7-Ec2SwGgEvxI",
+	authDomain: "ieee-custom-forms-181e4.firebaseapp.com",
+	projectId: "ieee-custom-forms-181e4",
+	storageBucket: "ieee-custom-forms-181e4.appspot.com",
+	messagingSenderId: "182981659642",
+	appId: "1:182981659642:web:5b996efc9b10edca543243",
+	measurementId: "G-NJTMLT75G5",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth=getAuth();
+const auth = getAuth();
+const db = getFirestore(app);
 
-export {app,auth}; 
+export { app, auth, db };
