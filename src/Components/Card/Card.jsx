93e0445fd8b04e47c30d8821c9d27792ff4d98f2
Vaibhav Aswal <vbhv.aswal@gmail.com>
2022-12-27 16:32:52 +0530

    fixed issues in my forms

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,27 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Card.css";
-import { cards1 } from '../Constants/dummydata';
 
-function Card(){
-    return(
-        <div>
-        {cards1.map((e,id)=>{return(<div key={id}>
-    <div className='card'>
-        <div className='cardleft'>
-            <p className='cardcontent'>Form Tittle - {e.FormTittle}</p>
-            <p className='cardcontent'>Form Creation Date - {e.FormCreationDate}</p>
-            <p className='cardcontent'>Number of responces - {e.Numberofresponces}</p>
-
-        </div>
-        <div className='cardright'>
-            <button className='openclose'>open</button>
-            <Link className="viewresponse" >view responses</Link>
-        </div>        
-    </div>
-</div>)})}
-        </div>
-
-    )
+function Card(e) {
+	return (
+		<div className="card">
+			<div className="cardleft">
+				<p className="cardcontent">Form Tittle - {e.FormTittle}</p>
+				<p className="cardcontent">Form Creation Date - {e.FormCreationDate}</p>
+				<p className="cardcontent">
+					Number of responces - {e.Numberofresponces}
+				</p>
+			</div>
+			<div className="cardright">
+				<div className="openclose">open</div>
+				<Link className="viewresponse">view responses</Link>
+			</div>
+		</div>
+	);
 }
-export default  Card;
+export default Card;
