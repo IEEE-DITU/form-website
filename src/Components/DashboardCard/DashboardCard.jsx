adbdd82c4ccca9e0c2d29b531c0f364f2be367eb
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-01-09 18:51:02 +0530

    added delete form and few bug fixes

--- src/Components/DashboardCard/DashboardCard.jsx
+++ src/Components/DashboardCard/DashboardCard.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, updateDoc, deleteDoc, getDoc } from "firebase/firestore";
+import { Modal } from "@mantine/core";
 import { db } from "../../Firebase";
-import React from "react";
+import { useAuth } from "../../context/AuthContext";
+import React, { useState } from "react";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import toast from "react-hot-toast";
 import "./DashboardCard.css";
 
 function DashboardCard(e) {
+	const { currentUser } = useAuth();
+	const [modalOpened, setModalOpened] = useState(false);
+	const [deletemodalOpened, setdeleteModalOpened] = useState(false);
 	const promise = () => {
 		return new Promise((resolve, reject) => {
 			const ref = doc(db, "forms", e.id);
@@ -30,50 +35,163 @@ function DashboardCard(e) {
 			},
 		});
 	};
-	return (
-		<div className="card">
-			<div className="cardleft">
-				<p className="cardcontent">
-					<b>Form Tittle</b> - {e.title}
-				</p>
-				<p className="cardcontent">
-					<b>Form Creation Date</b> - {e.createdAt}
-				</p>
-				<p className="cardcontent">
-					<b>Number of responces</b> - {"in dvlpmnt..."}
-				</p>
-			</div>
-			<div className="cardright">
-				<div className="responceCloserOpener">
-					<p className={e.acceptingResponses ? "open" : "closed"}>
-						{e.acceptingResponses ? "Open" : "Closed"}
-					</p>
-					<ToggleSwitch
-						id={`${e.id}responceAccSwitcher`}
-						checked={e.acceptingResponses}
-						onChange={handleChange}
-						small={true}
-					/>
-				</div>
 
-				<Link className="viewresponse" to={`/user/forms/checkresponse/${e.id}`}>
-					view responses
-				</Link>
+	const deleteForm = () => {
+		const promise = () => {
+			return new Promise((resolve, reject) => {
+				try {
+					const ref = doc(db, "forms", e.id);
+					const ref2 = doc(db, "responses", e.id);
+					const ref3 = doc(db, "users", currentUser.uid);
+					deleteDoc(ref).catch((err) => console.log(err, "ref"));
+					deleteDoc(ref2).catch((err) => console.log(err, "ref2"));
+					getDoc(ref3)
+						.then((snapshot) => {
+							const abc = snapshot.data();
+							const oldData = abc.forms;
+							const index = oldData.indexOf(e.id);
+							if (index > -1) {
+								oldData.splice(index, 1);
+							}
+							updateDoc(ref3, {
+								forms: [...oldData],
+							}).then(() => {
+								setdeleteModalOpened(false);
+								resolve();
+							});
+						})
+						.catch((err) => console.log(err, "ref3"));
+				} catch {
+					reject();
+				}
+			});
+		};
+
+		toast.promise(promise(), {
+			loading: "deleting...",
+			success: () => {
+				return "Deleted!";
+			},
+			error: (err) => {
+				return `${err}`;
+			},
+		});
+	};
+	return (
+		<>
+			<Modal
+				opened={modalOpened}
+				onClose={() => setModalOpened(false)}
+				title={e.title}
+			>
 				<div
-					onClick={() =>
-						toast.success(
-							<span>
-								To share <b>Copy link from address bar</b>
-							</span>
-						)
-					}
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						gap: "1rem",
+					}}
 				>
-					<Link className="viewresponse" to={`/form/${e.id}`}>
-						Preview / Get Link
-					</Link>
+					<input
+						style={{
+							whiteSpace: "nowrap",
+							overflow: "scroll",
+							cursor: "text",
+							border: "1px solid grey",
+							borderRadius: "5px",
+							padding: "0.25rem",
+							outline: "none",
+							background: "rgb(243, 243, 243)",
+						}}
+						onChange={(e) => e.preventDefault()}
+						value={`https://form-website-seven.vercel.app/form/${e.id}`}
+					/>
+					<div
+						className="modalButton"
+						onClick={() => {
+							navigator.clipboard
+								.writeText(`https://form-website-seven.vercel.app/form/${e.id}`)
+								.then(toast.success("link copied to clipboard"))
+								.catch((err) => toast.error(err));
+						}}
+					>
+						click to copy
+					</div>
+				</div>
+			</Modal>
+			<Modal
+				opened={deletemodalOpened}
+				onClose={() => setdeleteModalOpened(false)}
+				title="Confirm Deletion"
+			>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						gap: "1rem",
+					}}
+				>
+					<p style={{ fontWeight: "500" }}>
+						Are you sure want to delete {e.title} ?
+					</p>
+					<div style={{ display: "flex", gap: "1rem" }}>
+						<div className="modalButton" onClick={() => deleteForm()}>
+							Delete
+						</div>
+						<div
+							className="modalButton2"
+							onClick={() => setdeleteModalOpened(false)}
+						>
+							Cancel
+						</div>
+					</div>
+				</div>
+			</Modal>
+			<div className="card">
+				<div className="cardleft">
+					<p className="cardcontent">
+						<span style={{ fontWeight: "600" }}>Form Tittle</span> - {e.title}
+					</p>
+					<p className="cardcontent">
+						<span style={{ fontWeight: "600" }}>Form Creation Date</span> -{" "}
+						{e.createdAt}
+					</p>
+					<p className="cardcontent">
+						<span style={{ fontWeight: "600" }}>Number of responces</span> -{" "}
+						{"in dvlpmnt..."}
+					</p>
+				</div>
+				<div className="cardright">
+					<div className="responceCloserOpener">
+						<p className={e.acceptingResponses ? "open" : "closed"}>
+							{e.acceptingResponses ? "Open" : "Closed"}
+						</p>
+						<ToggleSwitch
+							id={`${e.id}responceAccSwitcher`}
+							checked={e.acceptingResponses}
+							onChange={handleChange}
+							small={true}
+						/>
+					</div>
+					<div>
+						<Link
+							className="viewresponse"
+							to={`/user/forms/checkresponse/${e.id}`}
+						>
+							<p className="cardLinks">view responses</p>
+						</Link>
+					</div>
+					<div>
+						<p className="cardLinks">
+							<span onClick={() => setModalOpened(true)}>Get Link </span>|
+							<span> Edit</span>
+						</p>
+					</div>
+					<div onClick={() => setdeleteModalOpened(true)}>
+						<p className="closed">Delete</p>
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 export default DashboardCard;
