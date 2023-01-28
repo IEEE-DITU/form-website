4ed4748154ffec2147099d3223de0a56d7201a58
shreyal0007 <shreyaljain0007@gmail.com>
2023-01-28 21:51:51 +0530

    changes in closed response page n submit response page

--- src/pages/Submitresponse/Submitresponse.jsx
+++ src/pages/Submitresponse/Submitresponse.jsx
@@ -4,18 +4,30 @@ import bgimage3 from "../../images/bgimage3.png";
 import "./Submitresponse.css"
 import {
 	doc,
+    getDoc,
 	
 } from "firebase/firestore";
 import { db } from "../../Firebase";
+import { ref } from "firebase/storage";
 
-const Submitresponse = () => {
+const Submitresponse = (e) => {
+   const promise = () => {
+		return new Promise((resolve, reject) => {
+			const ref = doc(db, "forms", e.id);
+			getDoc(ref)
+				.then(() => resolve())
+				.catch((err) => {
+					reject(err);
+				});
+		});
+	};
     
 return (
   
     <div classname="mainb" style={{backgroundImage:`url(${bgimage3})`,width:'100%',height:'100vh'}}>
         <div className='maindiv'>
             <div className="contentdiv">
-            <u className='underline'><p className="formname">Form name</p></u>
+            <u className='underline'><p className="formname">{ref.title}</p></u>
             <img className="clap" src ={hands1} alt="image not available" ></img>
             <p className="thankyou">THANK YOU</p>
             <p className="ressub">Your response has been submitted.</p>
