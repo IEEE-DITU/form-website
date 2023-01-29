5e4e2b045b447eae596ebb872d8c3f4fee9a22e2
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-01-29 13:50:56 +0530

    fixed form close page design

--- src/pages/Formclosed/Formclosed.jsx
+++ src/pages/Formclosed/Formclosed.jsx
@@ -1,35 +1,21 @@
 import sadsticker from "../../images/sadsticker.png";
 import React from "react";
-import bgimage3 from "../../images/authImg.png";
 import "./Formclosed.css";
 
 const Formclosed = () => {
 	return (
-		<div
-			classname="mainb"
-			style={{
-				backgroundImage: `url(${bgimage3})`,
-				width: "100%",
-				height: "100vh",
-			}}
-		>
-			<div className="maindiv">
-				<div className="contentdiv">
-					<u className="underline">
-						<p className="formname">Form name</p>
-					</u>
-					<img
-						className="clap"
-						src={sadsticker}
-						alt="image not available"
-					></img>
-					<p className="thankyou">AW SNAP!</p>
-					<p className="ressub">
-						Looks like the form you are trying to access is no longer accepting
-						responses.
-					</p>
-					<p className="links">*links and some other stuff</p>
-				</div>
+		<div className="submittedPage">
+			<div className="submittedContent">
+				<u className="underline">
+					<p className="formname">Form name</p>
+				</u>
+				<img className="clap" src={sadsticker} alt="image not available"></img>
+				<p className="thankyou">AW SNAP!</p>
+				<p className="ressub">
+					Looks like the form you are trying to access is no longer accepting
+					responses.
+				</p>
+				<p className="links">*links and some other stuff</p>
 			</div>
 		</div>
 	);
