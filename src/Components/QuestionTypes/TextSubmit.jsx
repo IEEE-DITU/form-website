997e91bbff0bf09abd45ccef0a331c25b3d6ee96
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-01-29 17:06:41 +0530

    improved textarea in submit form page

--- src/Components/QuestionTypes/TextSubmit.jsx
+++ src/Components/QuestionTypes/TextSubmit.jsx
@@ -1,17 +1,30 @@
 import "./QuestionTypes.css";
 
 const Text = ({ setAnsText, responses, questionID, max }) => {
+	function textAreaAdjust(e) {
+		const element = document.getElementById(`${questionID}text`);
+		element.style.height = "1px";
+		element.style.height = 5 + element.scrollHeight + "px";
+	}
 	return (
 		<div className="TextType">
-			<input
+			<textarea
+				id={`${questionID}text`}
 				type="text"
 				placeholder="Insert answer"
 				value={responses[questionID]}
 				onChange={(e) => {
 					e.preventDefault();
+					if (
+						!e.nativeEvent.data &&
+						e.nativeEvent.inputType !== "deleteContentBackward"
+					) {
+						return;
+					}
 					setAnsText(questionID, e.target.value);
 				}}
 				maxLength={max}
+				onKeyUp={() => textAreaAdjust()}
 			/>
 		</div>
 	);
