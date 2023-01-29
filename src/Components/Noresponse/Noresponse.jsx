e1f2c1879694c3cd81bb217a7a5007fa184e05bd
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-01-29 14:03:47 +0530

    removed colsole warnings and fixed no response design for summary respoces

--- src/Components/Noresponse/Noresponse.jsx
+++ src/Components/Noresponse/Noresponse.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import image1 from "../../images/noresponse.png"
-import "./Noresponse.css"
+import React from "react";
+import image1 from "../../images/noresponse.png";
+import "./Noresponse.css";
 const Noresponse = () => {
-  return (
-    <div>
-      <img className="image1" src={image1}></img>
-      <p>Seems like you haven't got any responses yet ;( you can wait or <a className='link' href="">share this form</a></p>
+	return (
+		<div
+			style={{
+				display: "flex",
+				width: "100%",
+				justifyContent: "center",
+				alignItems: "center",
+				flexDirection: "column",
+			}}
+		>
+			<img className="image1" src={image1} alt="empty"></img>
+			<p>
+				Seems like you haven't got any responses yet ;( you can wait or
+				<p className="link">share this form</p>
+			</p>
+		</div>
+	);
+};
 
-    </div>
-  )
-}
-
-export default Noresponse
+export default Noresponse;
